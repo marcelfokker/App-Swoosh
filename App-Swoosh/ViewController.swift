331902c3e542b5f9bb2928b1850486a3410e0c36
Marcel Fokker <marcel1@192.168.1.7>
2017-09-03 20:49:07 +0200

    test branch

--- App-Swoosh/ViewController.swift
+++ App-Swoosh/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Swoosh: UIImageView!
+    @IBOutlet weak var BgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
